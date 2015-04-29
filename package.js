7e18659cba6bbb87d9a78088dc7e8fc06757dd4e
Ryan Glover <me@ryanglover.net>
2015-04-29 10:54:43 -0500

    fix version mismatch in package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'themeteorchef:controller',
-  version: '1.0.1',
+  version: '1.0.3',
   summary: 'An API for organizing your template logic.',
   git: 'https://github.com/themeteorchef/controller',
   documentation: 'README.md'
